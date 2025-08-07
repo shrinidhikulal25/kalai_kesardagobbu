79c1d0817ae5954aee7cb754aa4c82b93eaf38a7
Shreenidhi <shrinidhikulal25@gmail.com>
2025-08-07 11:35:13 +0530

    s

---
+++ script.js
@@ -0,0 +1,21 @@
+// Highlight nav link on scroll
+window.addEventListener('scroll', () => {
+  const sections = document.querySelectorAll('section');
+  const navLinks = document.querySelectorAll('nav a');
+
+  let current = '';
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 60;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove('active');
+    if (link.getAttribute('href').includes(current)) {
+      link.classList.add('active');
+    }
+  });
+});
